5d837f511b5f3481a2a8d36c5a493e4bb339df7c
joaoGlima09 <joao.glima@sptech.school>
2024-11-10 23:27:05 -0300

    feat: :zap: adicionando rota de anotacoes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // var ambiente_processo = 'producao';
-var ambiente_processo = 'desenvolvimento';
+var ambiente_processo = "desenvolvimento";
 
-var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+var caminho_env = ambiente_processo === "producao" ? ".env" : ".env.dev";
 // Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
 // A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
 
@@ -9,18 +9,19 @@ require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
 var cors = require("cors");
-var path = require("path"); 
+var path = require("path");
 var PORTA_APP = process.env.APP_PORT;
 var HOST_APP = process.env.APP_HOST;
 
 var app = express();
 
-var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuario");
-var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
+// ---
+var indexRouter = require("./src/routes/index");
+var anotacaoRouter = require("./src/routes/anotacao");
+var usuarioRouter = require("./src/routes/usuario");
 var metricasRouter = require("./src/routes/metricas");
 
 app.use(express.json());
@@ -29,17 +30,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-app.use("/", indexRouter);
-app.use("/usuario", usuarioRouter);
-app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
+// ---
+app.use("/", indexRouter);
+app.use("/usuario", usuarioRouter);
+app.use("/anotacao", anotacaoRouter);
 app.use("/metricas", metricasRouter);
 
-
 app.listen(PORTA_APP, function () {
-    console.log(`
+  console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
     ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
     ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
